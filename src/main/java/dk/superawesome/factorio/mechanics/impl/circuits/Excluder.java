0bf3079f8af28d070d855b9e5ef2d9d6fd334737
Alek <alexander.soelvsten@gmail.com>
2024-08-25 20:36:39 +0200

    done with excluder

--- src/main/java/dk/superawesome/factorio/mechanics/impl/circuits/Excluder.java
+++ src/main/java/dk/superawesome/factorio/mechanics/impl/circuits/Excluder.java
@@ -1,18 +1,43 @@
 package dk.superawesome.factorio.mechanics.impl.circuits;
 
+import dk.superawesome.factorio.Factorio;
 import dk.superawesome.factorio.mechanics.Circuit;
 import dk.superawesome.factorio.mechanics.MechanicProfile;
 import dk.superawesome.factorio.mechanics.MechanicStorageContext;
 import dk.superawesome.factorio.mechanics.Profiles;
+import dk.superawesome.factorio.mechanics.routes.Routes;
 import dk.superawesome.factorio.mechanics.transfer.ItemCollection;
 import dk.superawesome.factorio.mechanics.transfer.ItemContainer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Excluder extends Circuit<Excluder, ItemCollection> implements ItemContainer {
 
+    private final List<ItemStack> filter = new ArrayList<>();
+
     public Excluder(Location loc, BlockFace rotation, MechanicStorageContext context) {
         super(loc, rotation, context);
+    }
+
+    @Override
+    public void onBlocksLoaded(Player by) {
+        Filter.loadItems(filter, (Sign) loc.getBlock().getRelative(rot).getState(), by, loc, this);
+    }
+
+    @EventHandler
+    public void onSignChange(SignChangeEvent event) {
+        if (event.getBlock().equals(loc.getBlock().getRelative(rot))) {
+            Bukkit.getScheduler().runTask(Factorio.get(), () -> Filter.loadItems(filter, (Sign) event.getBlock().getState(), event.getPlayer(), loc, this));
+        }
     }
 
     @Override
@@ -22,7 +47,13 @@ public class Excluder extends Circuit<Excluder, ItemCollection> implements ItemC
 
     @Override
     public boolean pipePut(ItemCollection collection) {
-        return false;
+        for (ItemStack filter : this.filter) {
+            if (collection.has(filter)) {
+                return false;
+            }
+        }
+
+        return Routes.startTransferRoute(loc.getBlock(), collection, this, false);
     }
 
     @Override
