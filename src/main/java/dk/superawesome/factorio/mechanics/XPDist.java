db18fc10596ac6558ce1f066016954fe58133ac0
Martin <77696248+martinpugdal@users.noreply.github.com>
2024-09-01 21:57:33 +0200

    XPDist refactor & ChestShop added as soft-depend

--- src/main/java/dk/superawesome/factorio/mechanics/XPDist.java
+++ src/main/java/dk/superawesome/factorio/mechanics/XPDist.java
@@ -5,27 +5,25 @@ import java.util.Random;
 public class XPDist {
 
     private final double[] amounts;
-
-    private int lastIndex;
+    private int lastIndex = -1;
 
     public XPDist(int amount, double rangeStart, double rangeEnd) {
-        if (amount == 0) {
-            throw new IllegalArgumentException("0 amount");
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+        if (rangeStart >= rangeEnd) {
+            throw new IllegalArgumentException("rangeStart must be less than rangeEnd");
         }
 
-        Random rand = new Random();
         amounts = new double[amount];
-        for (int i = 0; i < amount; i++) {
+        Random rand = new Random();
+
+        for (int i = 0; i < amount; i++)
             amounts[i] = rand.nextDouble(rangeStart, rangeEnd);
-        }
     }
 
     public double poll() {
-        lastIndex++;
-        if (lastIndex == amounts.length) {
-            lastIndex = 0;
-        }
-
+        lastIndex = (lastIndex + 1) % amounts.length;
         return amounts[lastIndex];
     }
 }
