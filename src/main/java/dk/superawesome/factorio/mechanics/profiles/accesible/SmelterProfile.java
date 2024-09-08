c235ad9f89197cdf78db2ce1dac5f2c5b5265c99
Alek <alexander.soelvsten@gmail.com>
2024-09-08 15:50:51 +0200

    smelter profiler changes

--- src/main/java/dk/superawesome/factorio/mechanics/profiles/accesible/SmelterProfile.java
+++ src/main/java/dk/superawesome/factorio/mechanics/profiles/accesible/SmelterProfile.java
@@ -112,10 +112,10 @@ public class SmelterProfile implements GuiMechanicProfile<Smelter> {
     public MechanicLevel.Registry getLevelRegistry() {
         return MechanicLevel.Registry.Builder
                 .make(5)
-                .setDescription(2, Arrays.asList("§eLager: 11 stacks §f-> §e15 stacks", "§eBrændstof: 9 stacks §f-> §e14 stacks", "§eIngredienser: 9 stacks §f-> §e16 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(19) + "ms §f(§e"+ getIncreaseDifference(20, 19, true) +"% hurtigere§f)"))
-                .setDescription(3, Arrays.asList("§eLager: 15 stacks §f-> §e22 stacks", "§eBrændstof: 14 stacks §f-> §e20 stacks", "§eIngredienser: 16 stacks §f-> §e24 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(18) + "ms §f(§e"+ getIncreaseDifference(20, 18, true) +"% hurtigere§f)"))
-                .setDescription(4, Arrays.asList("§eLager: 22 stacks §f-> §e32 stacks", "§eBrændstof: 20 stacks §f-> §e32 stacks", "§eIngredienser: 24 stacks §f-> §e36 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(16) + "ms §f(§e"+ getIncreaseDifference(20, 16, true) +"% hurtigere§f)"))
-                .setDescription(5, Arrays.asList("§eLager: 32 stacks §f-> §e64 stacks", "§eBrændstof: 32 stacks §f-> §e48 stacks", "§eIngredienser: 36 stacks §f-> §e54 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(14) + "ms §f(§e"+ getIncreaseDifference(20, 14, true) +"% hurtigere§f)"))
+                .setDescription(2, Arrays.asList("§eLager: 11 stacks §f-> §e15 stacks", "§eBrændstof: 9 stacks §f-> §e16 stacks", "§eIngredienser: 9 stacks §f-> §e16 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(19) + "ms §f(§e"+ getIncreaseDifference(20, 19, true) +"% hurtigere§f)"))
+                .setDescription(3, Arrays.asList("§eLager: 15 stacks §f-> §e22 stacks", "§eBrændstof: 16 stacks §f-> §e24 stacks", "§eIngredienser: 16 stacks §f-> §e24 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(18) + "ms §f(§e"+ getIncreaseDifference(20, 18, true) +"% hurtigere§f)"))
+                .setDescription(4, Arrays.asList("§eLager: 22 stacks §f-> §e32 stacks", "§eBrændstof: 24 stacks §f-> §e36 stacks", "§eIngredienser: 24 stacks §f-> §e36 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(16) + "ms §f(§e"+ getIncreaseDifference(20, 16, true) +"% hurtigere§f)"))
+                .setDescription(5, Arrays.asList("§eLager: 32 stacks §f-> §e64 stacks", "§eBrændstof: 36 stacks §f-> §e54 stacks", "§eIngredienser: 36 stacks §f-> §e54 stacks", "§eHastighed: " + ticksToMs(20) + "ms §f-> §e" + ticksToMs(14) + "ms §f(§e"+ getIncreaseDifference(20, 14, true) +"% hurtigere§f)"))
 
                 .mark(MechanicLevel.XP_REQUIRES_MARK, Array.fromData(1000d, 2500d, 5000d, 10000d))
                 .mark(MechanicLevel.LEVEL_COST_MARK, Array.fromData(4096d, 8384d, 18480d, 36200d))
@@ -124,7 +124,7 @@ public class SmelterProfile implements GuiMechanicProfile<Smelter> {
 
                 .mark(ItemCollection.CAPACITY_MARK, Array.fromData(11, 15, 22, 32, 64))
                 .mark(Smelter.INGREDIENT_CAPACITY_MARK, Array.fromData(9, 16, 24, 36, 54))
-                .mark(Smelter.FUEL_CAPACITY_MARK, Array.fromData(9, 14, 20, 32, 48))
+                .mark(Smelter.FUEL_CAPACITY_MARK, Array.fromData(9, 16, 24, 36, 54))
                 .build();
     }
 
